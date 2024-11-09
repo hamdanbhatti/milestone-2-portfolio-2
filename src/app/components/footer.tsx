67a4db8c6e9b55e77e33beeb42023624df9255fc
Muhammad Hamdan Bhatti <hamdanbutt991gmail.com>
2024-11-10 03:50:46 +0500

    New Changes

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,16 +4,23 @@ const Footer = () => {
    return (
       <section className="footer">
          <div className="social">
-            <a href="#">
+            <a
+               href="https://www.facebook.com/hamdanbhatti.hamdanbhatti.9"
+               target="_blank"
+               rel="noreferrer"
+            >
                <FaFacebook />
             </a>
-            <a href="#">
+            <a
+               href="https://www.linkedin.com/in/hamdan-b-340215270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+               target="_blank"
+               rel="noreferrer"
+            >
                <FaLinkedin />{" "}
             </a>
-            <a href="#">
+            <a href="https://github.com/hamdanbhatti" target="_blank" rel="noreferrer">
                <FaGithub />
             </a>
-            <a href="#"></a>
          </div>
          <p className="copyright">
             Copyright © 2024 by <span>Hamdan Bhatti</span> | All Rights Reserved.
